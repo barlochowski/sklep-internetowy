dda3bf531aecd85a18e048b5f3ec44a7e551859d
Bartłomiej Łochowski <40227654+barlochowski@users.noreply.github.com>
2025-09-02 20:14:39 +0200

    Delete scripts./order.js

--- scripts./order.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('order-form').addEventListener('submit', function(e) {
-  e.preventDefault();
-  alert('Zamówienie złożone! Dziękujemy!');
-  localStorage.removeItem('cart');
-});
